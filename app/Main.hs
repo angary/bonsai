9960b1403fae521ba776debc7bb727d600f4a418
Gary Sun <angary@canva.com>
2026-05-17 01:32:37 +1000

    basic arg parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,9 @@
-module Main where
+import System.Environment (getArgs)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- getArgs
+    case args of
+        (branch:files) -> putStrLn $ "Branch: " ++ branch ++ " Files: " ++ show files
+        []             -> putStrLn "Usage: bonsai add <branch> <files> ..."
+

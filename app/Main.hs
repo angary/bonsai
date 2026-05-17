cabd7488ca6b8d8eb3467146fb09b3e5b259c432
Gary Sun <angary@canva.com>
2026-05-17 12:19:28 +1000

    git tree parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,42 @@
+import Control.Monad (when)
 import System.Directory (doesDirectoryExist, listDirectory)
-import System.Exit (ExitCode(..))
+import System.Exit (ExitCode(..), exitFailure)
 import System.Environment (getArgs)
 import System.Process (readProcessWithExitCode)
+
+
+data TreeEntry = TreeEntry 
+    { mode :: String
+    , entryType :: String
+    , sha :: String
+    , path :: String
+    } deriving (Show)
 
 
 main :: IO ()
 main = do
     args <- getArgs
-    case args of
-        (branch:paths) -> run branch paths
-        [] -> putStrLn "usage: bonsai add <branch> <files> ..."
+    when (null args) $ do
+        putStrLn "usage: bonsai add <branch> <files> ..."
+        exitFailure
+    let (branch:paths) = args
+    when (null paths) $ do
+        putStrLn "error: no files specified"
+        exitFailure
+    run branch paths
 
 
 run :: String -> [String] -> IO ()
 run branch paths = do
     exists <- doesBranchExist branch
-    if exists
-        then putStrLn "error: branch exists"
-        else do
-            expandedPaths <- expandPaths paths
-            putStrLn $ show expandedPaths
-            shas <- mapM hashObject expandedPaths
-            putStrLn $ show shas
+    -- TODO: Handle existing branch?
+    when exists $ do
+        putStrLn "error: branch already exists"
+        exitFailure
+    expandedPaths <- expandPaths paths
+    shas <- mapM hashFile expandedPaths
+    tree <- getTree
+    putStrLn $ show tree
 
 
 doesBranchExist :: String -> IO Bool
@@ -32,15 +47,30 @@ doesBranchExist branch = do
 
 expandPaths :: [String] -> IO [String]
 expandPaths paths = concat <$> mapM expandPath paths
-    where
-        expandPath p = do
-            isDir <- doesDirectoryExist p
-            if isDir
-                then expandPaths . map ((p ++ "/") ++) =<< listDirectory p
-                else return [p]
 
 
-hashObject :: String -> IO String
-hashObject paths = do
-    (_, stdout, _) <- readProcessWithExitCode "git" ["hash-object", "-w", paths] ""
+expandPath :: String -> IO [String]
+expandPath p = do
+    isDir <- doesDirectoryExist p
+    if isDir
+        then expandPaths . map ((p ++ "/") ++) =<< listDirectory p
+        else return [p]
+
+
+hashFile :: String -> IO String
+hashFile path = do
+    (_, stdout, _) <- readProcessWithExitCode "git" ["hash-object", "-w", path] ""
     return $ head $ lines stdout
+
+
+getTree :: IO [TreeEntry]
+getTree = do -- TODO: Look into not hardcoding origin/main
+    (_, stdout, _) <- readProcessWithExitCode "git" ["ls-tree", "origin/main"] ""
+    return $ map parseTreeEntry $ lines stdout
+
+
+parseTreeEntry :: String -> TreeEntry
+parseTreeEntry line =
+    let (meta, tabPath) = break (== '\t') line
+        [m, t, s]       = words meta
+    in TreeEntry m t s (drop 1 tabPath)

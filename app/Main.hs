089626dec1ced15fe8c8de1b9ef7eaa8aa15b325
Gary Sun <angary@canva.com>
2026-05-17 23:05:28 +1000

    MVP with commit and push

--- app/Main.hs
+++ app/Main.hs
@@ -20,6 +20,7 @@ main = do
         putStrLn "usage: bonsai add <branch> <files> ..."
         exitFailure
     let (branch:paths) = args
+    -- TODO: Auto add modified files to new branch if null?
     when (null paths) $ do
         putStrLn "error: no files specified"
         exitFailure
@@ -34,9 +35,15 @@ run branch paths = do
         putStrLn "error: branch already exists"
         exitFailure
     expandedPaths <- expandPaths paths
-    shas <- mapM hashFile expandedPaths
-    tree <- getTree
-    putStrLn $ show tree
+    shas          <- mapM hashFile expandedPaths
+    tree          <- getTree
+    let finalTree  = buildTree expandedPaths shas tree
+    let treeInput  = unlines $ map formatTreeEntry finalTree
+    treeSha       <- mkTree treeInput
+    commitSha     <- createCommit treeSha
+    createBranch branch commitSha
+    push branch
+    putStrLn $ "pushed branch: " ++ branch
 
 
 doesBranchExist :: String -> IO Bool
@@ -63,8 +70,10 @@ hashFile path = do
     return $ head $ lines stdout
 
 
+-- TODO (IMPORTANT): Support subdirectories
+-- TODO: Investigate non-hardcoded origin/main
 getTree :: IO [TreeEntry]
-getTree = do -- TODO: Look into not hardcoding origin/main
+getTree = do
     (_, stdout, _) <- readProcessWithExitCode "git" ["ls-tree", "origin/main"] ""
     return $ map parseTreeEntry $ lines stdout
 
@@ -74,3 +83,51 @@ parseTreeEntry line =
     let (meta, tabPath) = break (== '\t') line
         [m, t, s]       = words meta
     in TreeEntry m t s (drop 1 tabPath)
+
+
+buildTree :: [String] -> [String] -> [TreeEntry] -> [TreeEntry]
+buildTree paths shas tree = 
+    let pathShas    = zip paths shas
+        updatedTree = map (updateTreeEntry pathShas) tree
+        newEntries  = [TreeEntry "100644" "blob" s p | (p, s) <- pathShas, p `notElem` map path tree]
+    in updatedTree ++ newEntries
+
+
+updateTreeEntry :: [(String, String)] -> TreeEntry -> TreeEntry
+updateTreeEntry pathShas entry =
+    case lookup (path entry) pathShas of
+        Just newSha -> entry { sha = newSha }
+        Nothing     -> entry
+
+
+formatTreeEntry :: TreeEntry -> String
+formatTreeEntry e  = mode e ++ " " ++ entryType e ++ " " ++ sha e ++ "\t" ++ path e
+
+
+mkTree :: String -> IO String
+mkTree treeInput = do
+    (_, stdout, _) <- readProcessWithExitCode "git" ["mktree"] treeInput
+    return $ head $ lines stdout
+
+
+createCommit :: String -> IO String
+createCommit treeSha = do
+    -- TODO: Current default commit message is "." (my convention), can open to user input
+    (_, stdout, _) <- readProcessWithExitCode "git" ["commit-tree", treeSha, "-p", "origin/main", "-m", "."] ""
+    return $ head $ lines stdout
+
+
+createBranch :: String -> String -> IO ()
+createBranch branch commitSha = do
+    (code, stdout, stderr) <- readProcessWithExitCode "git" ["update-ref", "refs/heads/" ++ branch, commitSha] ""
+    putStr stdout
+    putStr stderr
+    when (code /= ExitSuccess) exitFailure
+
+
+push :: String -> IO ()
+push branch = do
+    (code, stdout, stderr) <- readProcessWithExitCode "git" ["push", "origin", branch] ""
+    putStr stdout
+    putStr stderr
+    when (code /= ExitSuccess) exitFailure
